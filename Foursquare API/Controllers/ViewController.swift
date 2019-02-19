7189836dc3d3e9623a58dec5dd8348a42b602173
Student <student@3-pk-u-okna.local>
2019-02-19 17:01:13 +0300

    Initial Commit

---
+++ Foursquare API/Controllers/ViewController.swift
@@ -0,0 +1,98 @@
+//
+//  ViewController.swift
+//  Foursquare API
+//
+//  Created by Denis Bystruev on 22/01/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import MapKit
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var topStackView: UIStackView!
+    @IBOutlet weak var mapView: MKMapView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    let url = URL(string: "https://api.foursquare.com/v2/venues/search")!
+    
+    var controller: VenueTableViewController?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        mapView.delegate = self
+        loadData()
+    }
+    
+    func loadData() {
+        
+        var urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false)!
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "client_id", value: "U34GZW25B2ZJ5BPYHOWSYBFHI1XZFEV30EHLAADCFKM1REWF"),
+            URLQueryItem(name: "client_secret", value: "RUF2RIAEG2ORYHPTORDHBMRL0OKNJQKTC3QMG5TM3SESR0AH"),
+            URLQueryItem(name: "ll", value: "55.751857, 37.666629"),
+            URLQueryItem(name: "v", value: "20190205"),
+        ]
+        
+        let requestURL = urlComponents.url!
+        
+        URLSession.shared.dataTask(with: requestURL) { data, _, _ in
+            
+            guard let data = data else {
+                print(#function, "ERROR: Can't load data from \(requestURL.absoluteString)")
+                return
+            }
+            
+            guard let venues = Response(data: data) else {
+                print(#function, "ERROR: List of venues is empty")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                let venues = venues.response.venues
+                
+                self.navigationItem.title = "Venues loaded: \(venues.count)"
+                
+                self.mapView.showAnnotations(venues, animated: true)
+            }
+            
+        }.resume()
+    }
+}
+
+extension ViewController {
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        
+        if size.height < size.width {
+            topStackView.axis = .horizontal
+        } else {
+            topStackView.axis = .vertical
+        }
+    }
+}
+
+extension ViewController: MKMapViewDelegate {
+    func mapView(_ mapView: MKMapView, didSelect view: MKAnnotationView) {
+        
+        mapView.deselectAnnotation(view.annotation, animated: true)
+        
+        guard let venue = view.annotation as? Venue else { return }
+        
+        controller?.venue = venue
+    }
+}
+
+// MARK: - ...Navigation
+extension ViewController {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard segue.identifier == "VenueSegue" else { return }
+        
+        guard let controller = segue.destination as? VenueTableViewController else
+            { return }
+        
+        self.controller = controller
+    }
+}
---
+++ Foursquare API/Models/Venue.swift
@@ -0,0 +1,81 @@
+//
+//  Venue.swift
+//  Foursquare API
+//
+//  Created by Denis Bystruev on 29/01/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import MapKit
+
+struct Response: Codable {
+    let response: Venues
+    
+    init?(data: Data?) {
+        guard let data = data else { return nil }
+        
+        let decoder = JSONDecoder()
+        
+        guard let response = try? decoder.decode(Response.self, from: data) else { return nil }
+        
+        self.response = response.response
+    }
+}
+
+struct Venues: Codable {
+    let venues: [Venue]
+}
+
+class Venue: NSObject, Codable {
+    let name: String
+    let location: Location
+    let desc: String?
+    let categories: [Category]
+    
+    enum CodingKeys: String, CodingKey {
+        case name
+        case location
+        case desc = "description"
+        case categories
+    }
+}
+
+struct Location: Codable {
+    let address: String?
+    let lat: Double
+    let lng: Double
+}
+
+struct Category: Codable {
+    let id: String
+    let name: String
+    let icon: Icon
+}
+
+struct Icon: Codable {
+    let prefix: String
+    let suffix: String
+    
+    func fetch(completion: @escaping (UIImage?) -> Void) {
+        let name = "\(prefix)88\(suffix)"
+        
+        guard let url = URL(string: name) else { return }
+        
+        URLSession.shared.dataTask(with: url) { data, _, _ in
+            guard let data = data else { return }
+            let image = UIImage(data: data)
+            
+            completion(image)
+        }.resume()
+    }
+}
+
+extension Venue: MKAnnotation {
+    var coordinate: CLLocationCoordinate2D {
+        return CLLocationCoordinate2D(latitude: location.lat, longitude: location.lng)
+    }
+    
+    var title: String? {
+        return name
+    }
+}
